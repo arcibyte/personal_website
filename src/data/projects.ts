0352cb17ed1d37ef9be5146674e6932bfa036a20
arcibyte <arcibyte@gmail.com>
2025-05-25 17:35:43 -0500

    ♻️  refactor: simplify projects list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,50 +1,35 @@
-
 export interface Project {
   title: string;
   description: string;
-  span: string;
-  slug: string;
-  details?: string;
+  projectUrl: string;
+  githubUrl?: string;
 }
 
 export const projects: Project[] = [
   {
     title: "Proyecto 1",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    span: "col-span-2 row-span-1",
-    slug: "proyecto-1",
-    details: "ishgoneñlgdsnñgsalngñaewlsnsgakñjzxngasñonjñaglsngñjoweqangrñojqewnañsjogk nerñjkagsbgñaewuobsjsgka hweñuoafebsnlgkhebadsvnño iw woehafsknñguoqejkrabñqewrabgspuo bewhqpiqeglhu34qwpeuibgeqpasidulbgeaiupgbleiugqbwgrli"
+    projectUrl: "https://proyecto1.com",
+    githubUrl: "https://github.com/"
   },
   {
     title: "Proyecto 2",
     description: "Adaptación de equipos",
-    span: "col-span-1 row-span-1",
-    slug: "proyecto-2",
-    details: "Detalles del Proyecto 2"
+    projectUrl: "https://proyecto2.com",
+    githubUrl: "https://github.com/"
   },
   {
     title: "Proyecto 3",
     description: "Sincronización y coordinación del equipo.",
-    span: "col-span-1 row-span-2",
-    slug: "proyecto-3"
+    projectUrl: "https://proyecto3.com",
+    githubUrl: "https://github.com/"
   },
   {
     title: "Proyecto 4",
     description: "Descripción breve.",
-    span: "col-span-1 row-span-1",
-    slug: "proyecto-4"
-  },
-  {
-    title: "Proyecto 5",
-    description: "Conectando personas y soluciones.",
-    span: "col-span-2 row-span-1",
-    slug: "proyecto-5"
-  },
-  {
-    title: "Proyecto 6",
-    description: "Hola, ¿dónde estás?",
-    span: "col-span-2 md:col-span-4 row-span-1",
-    slug: "proyecto-6"
-  },
+    projectUrl: "https://proyecto4.com",
+    githubUrl: "https://github.com/"
 
+
+  }
 ];
